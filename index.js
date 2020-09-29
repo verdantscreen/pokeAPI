d36a23ad7960731424abda1a7e4dd2368c15b010
verdantscreen <verdantscreen2020@gmail.com>
2020-09-28 20:31:27 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+let pokeId;
+let pokeInfo = document.querySelector("section");
+// search form/input
+const pokeNumber = document.getElementById("search");
+const pokeForm = document.querySelector("form");
+const section = document.querySelector("section");
+pokeForm.addEventListener("submit", fetchPoke);
+function fetchPoke(e) {
+  e.preventDefault();
+  let pokeName = pokeNumber.value;
+  fetch(`${baseURL}${pokeName}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      displayPoke(json);
+    })
+    .catch((err) =>
+      alert("You entered an invalid Pokémon name or number! Please try again.")
+    );
+}
+function displayPoke(json) {
+  while (section.firstChild) {
+    section.removeChild(section.firstChild);
+  }
+  let poke = json;
+  let name = poke.name;
+  let imageURL = poke.sprites.front_default;
+  let height = poke.height;
+  let weight = poke.weight;
+  let inputName = document.createElement("h2");
+  inputName.innerHTML = name;
+  pokeInfo.appendChild(inputName);
+  let inputPic = document.createElement("div");
+  inputPic.innerHTML = `<img src= ${imageURL}>`;
+  pokeInfo.appendChild(inputPic);
+  let inputHgtWgt = document.createElement("h4");
+  inputHgtWgt.innerHTML = `${height} dm ${weight} hg`;
+  pokeInfo.appendChild(inputHgtWgt);
+}
